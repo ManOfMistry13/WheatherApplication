2c491c2a2b38f1ab56d1d5b3312f851c69f89109
amine <Vous@exemple.com>
2023-01-02 16:59:59 +0100

    Initial commit

---
+++ app/src/main/java/datatype/CodePostal.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class CodePostal {
+
+    private int code;
+
+    public CodePostal(int code) {
+        this.code=code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
